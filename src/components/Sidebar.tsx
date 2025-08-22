f1c54b1209529365739148cb2235df5566235898
namu <southelp@naver.com>
2025-08-22 21:32:35 +0900

    16

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -70,7 +70,7 @@ const Sidebar = forwardRef<HTMLDivElement, SidebarProps>(({ isCollapsed }, ref) 
       ref={ref}
       className={`
         flex flex-col h-full
-        ${isCollapsed ? 'w-[108px]' : 'w-[330px]'}
+        ${isCollapsed ? 'w-[81px]' : 'w-[248px]'}
         transition-all duration-200 ease-out
         border-r
         relative
@@ -79,7 +79,7 @@ const Sidebar = forwardRef<HTMLDivElement, SidebarProps>(({ isCollapsed }, ref) 
         borderColor: '#EEEEEC',
         background: '#F8F8F7'
       }}
-      animate={{ width: isCollapsed ? 108 : 330 }}
+      animate={{ width: isCollapsed ? 81 : 248 }}
       transition={{ duration: 0.2, ease: 'easeOut' }}
     >
       {/* Logo Section */}
