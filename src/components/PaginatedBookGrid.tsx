1076bd578a664c7f7d9b9d59bf8d56c8c952a6d5
namu <southelp@naver.com>
2025-08-23 06:39:23 +0900

    8

--- src/components/PaginatedBookGrid.tsx
+++ src/components/PaginatedBookGrid.tsx
@@ -24,7 +24,7 @@ const PaginatedBookGrid = <T,>({ items, renderItem, itemsPerPage = 20 }: Paginat
 
   return (
     <div>
-      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-8 gap-4">
+      <div className="grid grid-cols-7 gap-4">
         {currentItems.map((item, index) => (
           <React.Fragment key={index}>
             {renderItem(item)}
